35a517ef220d1cf501d7e36e45392aa71aa6fc9e
andresedal <andresedeabreul@gmail.com>
2021-10-21 13:18:39 +0200

    pagina web(php) 4

---
+++ web1/prueba.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>WEB 1</title>
+    <link rel="stylesheet" href="css/estilos_prueba.css">
+  </head>
+  <body>
+    <pre>
+      Ejecuta php -S localhost:9000
+      visita con tu navegador
+    </pre>
+    <h1>Título 1</h1>
+    <p>Párrafo corto 1</p>
+    
+    <?php
+      echo "Esto es de PHP <b>PHP</b>"
+    ?>
+  </body>
+</html>
