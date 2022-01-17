2772c3d37b5ab59d5b8447616c2c84ba596d7dcd
andresedal <andresedeabreul@gmail.com>
2022-01-17 12:18:09 +0100

    actualizacion

---
+++ web1/intro/prueba.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>WEB 1</title>
+    <link rel="stylesheet" href="css/estilos_prueba.css">
+  </head>
+  <body>
+    <div id="cabecera">
+      <h1>Esto es mi cabecera</h1>
+      <?php
+        echo $miprimeravariable;
+      ?>
+    </div>
+    <div>
+      <h1>Titulo</h1>
+      <img src="https://picsum.photos/100/150" alt="">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada ipsum eget varius bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc vel massa sed lacus dapibus placerat non fermentum lacus. Proin faucibus dui at mauris tempus, vehicula pharetra enim fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel nisi ut turpis pellentesque rhoncus. Suspendisse mattis varius posuere. Nulla aliquam libero nec faucibus dapibus. Duis vulputate viverra tellus ut tincidunt. Integer vulputate ex et est sollicitudin placerat. Aenean ornare sem et nisi faucibus iaculis. Aenean at congue leo. Nam eget odio cursus, feugiat velit a, suscipit velit.
+    </div>
+  </body>
+</html>
